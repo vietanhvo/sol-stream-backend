27673eeac6b373f6643142283f3979d19a8b5704
Anh Viet <anhviet5121pc@gmail.com>
2022-08-03 17:20:20 +0700

    Update db

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,22 @@
+#[macro_use]
+extern crate diesel;
+
+mod models;
+mod routes;
+mod schema;
+mod solana;
+
+use diesel::prelude::*;
+use dotenv::dotenv;
 use rocket::{get, routes};
+use std::env;
+
+pub fn establish_connection() -> MysqlConnection {
+    dotenv().ok();
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    MysqlConnection::establish(&database_url)
+        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+}
 
 #[rocket::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
